d2c231e965c221bc0e3f066d90fbdecd343f82d1
Dave Hampton <info@clearspotmedia.co.uk>
2023-04-14 12:28:03 +0100

    David Hampton, theme iteration

--- js/design-system.js
+++ js/design-system.js
@@ -1,56 +1,88 @@
-/**
- * JQuery file for design system contents menu and LocalGov Search customisations.
- */
+//Automatic page contents menu for sidebar - inserts anchor link and title in contents menu, for each page section that starts with a h2
+document.addEventListener('DOMContentLoaded', function (event) {
 
-jQuery(function ($) {
-//Automatic menu from li
+  //Only fire if we have our custom paragraph types on the page
+  var elem = document.querySelector(".paragraph--type--cds-design-system-text-section h2");
+  if (elem) {
 
-if ($(".page-node-type-design-system-page .field__item h2")[0]) {
-var isparent = $('.ds-menu__secondary .menu-item.menu-item--active-trail');// Find active item parent link IDs
-var insert_content_top = $('.design-system-page__content');// Insert contents menu here on mobile
-// H2 headings exist so make the anchor menu
-$(isparent).append('<ul id="ds-anchor-menu"></ul>');// Make ul container for menu inside
-$(insert_content_top).prepend('<div id="ds-contents"><h2 id="ds-contents__heading">Contents</h2><ul></ul></div>');// Make ul container for menu inside
+    //Target elements for current page link, and design system content wrapper
+    var parent = document.querySelector(".ds-menu__secondary .nav-item.is-active-trail a"); //Current active page link in contents menu
+    var contents_mob = document.querySelector(".cds-design-system__content"); // Design system page content wrapper
+    // Create a wrapper for our auto menu and insert it after the current page link
+    var parent_content = '<ul id="ds-anchor-menu"></ul>';
+    parent.insertAdjacentHTML('afterend', parent_content);
+    // Create a wrapper for our mobile contents menu, insert it at the start of the design system content (mobile only CSS)
+    var contents_mob_content = '<div id="ds-contents"><ul></ul></div>';
+    contents_mob.insertAdjacentHTML('afterbegin', contents_mob_content);
 
-// Find active page in design system menu, loop out the page anchors based on h2 IDs
-var isactive = $('.ds-menu__secondary a.is-active');//Active menu link
-var ismenu = $('#ds-anchor-menu');//Anchor menu wrapper
-$(".page-node-type-design-system-page .field__item h2:not(#ds-contents__heading)").each(function () {//Get each h2 text
-  var thetitle = $(this).text();
-  var hyphenatedtitle = thetitle.replace(/ /g, "-");//Hyphenate h2 title for use in anchor link
-  $(this).prepend('<a name="' + hyphenatedtitle + '"></a>');//Create anchor before h2
-  $(ismenu).append('<li><a href="#' + hyphenatedtitle + '" class="ds-anchorlink">- ' + $(this).text() + '</a></li>');//Output each link in design system menu
-  $('#ds-contents ul').append('<li><a href="#' + hyphenatedtitle + '" class="ds-anchorlink">- ' + $(this).text() + '</a></li>');//Output each link in the top of page contents menu
-});
-} else {
-// Do something if class does not exist
-}
+    //Target active link and wrapper for auto menu
+    var isactive = document.querySelector('.ds-menu__secondary .nav-item.is-active-trail a'); //Current active page link in contents menu
 
-// Highlight the active page links in the design system menu (all levels). 
-//Uses 'is-active' class which was already in place for top level menu items but had no styling assigned
-var path = window.location.href; // Get the url 
-$('.ds-menu__secondary a').each(function () {
-if (this.href == path) {// If link URL matches browser URL make active
-  $(this).attr('class', 'is-active');
-}
-else { }
+    var anchor_menu_wrapper = document.getElementById('ds-anchor-menu'); //Anchor menu wrapper we created previously using parent_conten
+
+    //Create the anchor menu
+    [...document.querySelectorAll('.paragraph--type--cds-design-system-text-section h2')].forEach(el => {
+      //Set up hyphenated title
+      var thetitle = el.innerText; //h2 titles
+      var hyphenatedtitle = thetitle.replace(/ /g, "-").toLowerCase(); //Hyphenate h2 title for use in anchor link
+      var contents_mob_wrapper = document.querySelector("#ds-contents ul"); // Design system page wrapper 
+      var anchorlinks = '<li><a href="#' + hyphenatedtitle + '" class="ds-anchorlink">- ' + thetitle + '</a></li>';
+
+      anchor_menu_wrapper.insertAdjacentHTML('beforeend', anchorlinks); //Assign links to anchor menu
+      contents_mob_wrapper.insertAdjacentHTML('beforeend', anchorlinks); //Assign links to contents menu mobile
+    })
+  }
 });
 
-// Highlight the active anchor link in the design system menu when clicked
-$('.ds-menu__secondary a').on('click', function (e) {
-$('.ds-menu__secondary a').each(function () {
-  $(this).removeClass('is-active');
-})
-$(this).addClass('is-active');
+// Highlight the active 'anchor' links in the design system menu when clicked
+document.addEventListener('click', function (event) {
+  // If the clicked element doesn't have the right selector, bail
+  if (!event.target.matches('.ds-anchorlink')) return;
+  //Remove active class from all menu items
+  [...document.querySelectorAll('.ds-anchorlink')].forEach(el => {
+    el.classList.remove("is-active");
+  })
+  //Add active class to clicked link
+  event.target.classList.add('is-active');
+  // Log the clicked element in the console
+}, false);
+
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  //Look for contents menu, only run the functions if it exists
+  var elem = document.querySelector(".ds-menu__secondary");
+  if (elem) {
+    //Highlight the active page in the contents menu
+    var path = window.location.href; // Get the url 
+
+    [...document.getElementsByClassName('.ds-menu__secondary a')].forEach(el => {
+      if (event.currentTarget.href == path) {
+        event.currentTarget.classList.add(".is-active")
+      } else { }
+    })
+  }
 });
 
-// Change the wording of the search field placeholder in the header bar and search page
-$('#views-exposed-form-localgov-sitewide-search-sitewide-search-page-block, #views-exposed-form-localgov-sitewide-search-sitewide-search-page').find("input[type=text]").each(function (ev) {
-$(this).attr("placeholder", "Search design system");
-});
-// Change the wording of the search submit button in the header bar and search page
-$('#views-exposed-form-localgov-sitewide-search-sitewide-search-page-block, #views-exposed-form-localgov-sitewide-search-sitewide-search-page').find("input[type=submit]").each(function (ev) {
-$(this).val('Find');
+//Change wording on LocalGov Search module header search
+document.addEventListener('DOMContentLoaded', function () {
+  //Header LocalGov Search container (present in header only)
+  var elem = document.getElementById('views-exposed-form-cds-design-system-group-search-search-page');
+  if (elem) {
+    // Change the wording of the search submit button in the header bar
+    document.getElementById('views-exposed-form-cds-design-system-group-search-search-page').querySelector("input[type=submit]").value = 'Search';
+    // Change the wording of the search field placeholder in the header bar 
+    document.getElementById('views-exposed-form-cds-design-system-group-search-search-page').querySelector("input[type=text]").placeholder = 'Search design system';
+  }
 });
 
+//Change wording on LocalGov Search module page search
+document.addEventListener('DOMContentLoaded', function () {
+  //LocalGov Search Form container (present in both header and page)
+  var elem = document.getElementById('views-exposed-form-localgov-sitewide-search-sitewide-search-page');
+  if (elem) {
+    // Change the wording of the search submit button in the search page
+    document.getElementById('views-exposed-form-localgov-sitewide-search-sitewide-search-page').querySelector("input[type=submit]").value = 'Search';
+    // Change the wording of the search field label in the search page
+    document.getElementById('views-exposed-form-localgov-sitewide-search-sitewide-search-page').querySelector("label").innerText = 'Search design system';
+  }
 });
